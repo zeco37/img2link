85690254aa20525e973c44be8142d98724f8b0ac
zakaria belalioui <zecodscrd@gmail.com>
2026-02-09 15:13:15 +0100

    Update img2link.py

--- img2link.py
+++ img2link.py
@@ -47,7 +47,7 @@ def _data_uri_from_source(src: str) -> Optional[str]:
 
 ICON = _pil_icon_from_source(LOGO_SOURCE)
 st.set_page_config(
-    page_title="Image 2 Link",
+    page_title="But Why...!",
     page_icon=ICON if ICON else "🔗",
     layout="centered",
 )
